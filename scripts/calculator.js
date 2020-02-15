4667bc3563c831c8daa05873e3be67e859a0bee3
alex-burman <alexburman90@gmail.com>
2020-02-15 09:48:25 -0700

    refactored add, subtract, multiply, divide, and operate functions to be object methods. It looked more organized.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,20 +1,51 @@
-console.log('Hello, world!');
-const add = function (a, b) {
-    return a + b;
-}
+// const add = function (a, b) {
+//     return a + b;
+// }
 
-const subtract = function (a, b) {
-    return a - b;
-}
+// const subtract = function (a, b) {
+//     return a - b;
+// }
 
-const multiply = function (a, b) {
-    return a * b;
-}
+// const multiply = function (a, b) {
+//     return a * b;
+// }
 
-const divide = function (a, b) {
-    return a / b;
-}
+// const divide = function (a, b) {
+//     return a / b;
+// }
 
-const test = function (func, a, b) {
-    console.log(func(a, b));
+// const operate = function (operator, a, b) {
+//     if (operator == 'add') {
+//         return add(a, b);
+//     } else if (operator == 'subtract') {
+//         return subtract(a, b);
+//     } else if (operator == 'multiply') {
+//         return multiply(a, b);
+//     } else if (operator == 'divide') {
+//         return divide(a, b);
+//     } else {
+//         console.log('invalid operator');
+//     }
+// }
+
+const operators = {
+    add(a, b) {
+        return a + b;
+    },
+    subtract(a, b) {
+        return a + b;
+    },
+    multiply(a, b) {
+        return a * b;
+    },
+    divide(a, b) {
+        return a / b;
+    },
+    operate(operator, a, b) {
+        if (operator in this) {
+            return this[operator](a, b);
+        } else {
+            console.log('invalid operator');
+        }
+    },
 }
