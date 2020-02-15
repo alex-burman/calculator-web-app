18bf7a3e7dcc5956c4c4fca471b84bb1a2f3ed97
alex-burman <alexburman90@gmail.com>
2020-02-14 18:40:10 -0700

    created add, subtract, multiply, and divide functions

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,20 @@
 console.log('Hello, world!');
+const add = function (a, b) {
+    return a + b;
+}
+
+const subtract = function (a, b) {
+    return a - b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
+
+const test = function (func, a, b) {
+    console.log(func(a, b));
+}
