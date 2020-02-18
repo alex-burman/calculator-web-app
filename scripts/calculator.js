a111b85d2af31542c9ff6395baa8d540ac268573
alex-burman <alexburman90@gmail.com>
2020-02-17 21:14:46 -0700

    added ability to evaluate paretheses to 'evaluate' function. Created function 'prepLine' which takes a string and coverts it to an array that the 'evaluate' function can understand.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,33 +1,3 @@
-// const add = function (a, b) {
-//     return a + b;
-// }
-
-// const subtract = function (a, b) {
-//     return a - b;
-// }
-
-// const multiply = function (a, b) {
-//     return a * b;
-// }
-
-// const divide = function (a, b) {
-//     return a / b;
-// }
-
-// const operate = function (operator, a, b) {
-//     if (operator == 'add') {
-//         return add(a, b);
-//     } else if (operator == 'subtract') {
-//         return subtract(a, b);
-//     } else if (operator == 'multiply') {
-//         return multiply(a, b);
-//     } else if (operator == 'divide') {
-//         return divide(a, b);
-//     } else {
-//         console.log('invalid operator');
-//     }
-// }
-
 const operators = {
     add(a, b) {
         return a + b;
@@ -42,10 +12,8 @@ const operators = {
         return a / b;
     },
     operate(operator, a, b) {
-        console.log('test',operator, a, b);
         if (operator in this) {
             let temp = this[operator](a, b);
-            console.log('This is the operate function result', temp)
             return temp;
         } else {
             console.log('invalid operator');
@@ -87,31 +55,91 @@ keys.addEventListener('click', e => {
 
 let testA = ['2', 'multiply', '2', 'add', '3', 'multiply', '4']
 let testB = ['2','add','2','add','2','divide','2'];
-let testC = ['4', 'add', '2','multiply','3'];
+let testC = ['4', 'add', '2','multiply','3','divide','2','subtract','20'];
+let testD = ['(','2',')','multiply','(','(','2',')',')'];
+let testE = ['(','(','2',')',')'];
+let testF = ['8', 'multiply', '(','2','divide','(','2','divide','2',')',')']
 
+let testStr = '8(8*2)+2';
+let testStrA = '(8+((2))*(2)/(2)+1)'
 
-const evaluate= function (arr) {
-    if (arr.length < 2) return arr[0];
+const testEval = function (str) {
+    let result = evaluate(prepLine(str));
+    console.log(result);
+}
 
-    let opIndex = arr.length - 2;
+const prepLine = function (str) {
+    let prepped = [];
 
-    if (arr.includes('add') || arr.includes('subtract')) {
-        for (let i = (arr.length - 1); i >= 0; i--) {
-            let item = arr[i];
-            if (item == 'add' || item == 'subtract') {
-                opIndex = i;
+    str.split('').forEach((item, index, arr) => {
+        let addItem;
+
+        if (
+            index > 0 &&
+            item == '(' &&
+            (/[0-9]/.test(item) || arr[index - 1] == ')')
+        ) {
+            addItem = ['multiply',item];
+        } else if (item == '+') {
+            addItem = 'add';
+        } else if (item == '-') {
+            addItem = 'subtract';
+        } else if (item == '*') {
+            addItem = 'multiply';
+        } else if (item == '/') {
+            addItem = 'divide';
+        } else {
+            addItem = item;
+        }
+
+        prepped = prepped.concat(addItem);
+    })
+    return prepped;
+}
+
+const evaluate = function (arr) {
+    while (arr.includes('(') && arr.includes(')')) {
+        let open = 0;
+        let close = 0;
+        let start = arr.indexOf('(');
+        let group;
+
+        for (let i = start; i < arr.length; i++) {
+            if (arr[i] == '(') {
+                open++;
+            }
+
+            if (arr[i] == ')') {
+                close++;
+            }
+
+            if (open == close) {
+                group = arr.slice(start + 1, i);
+                arr.splice(start, i - start + 1, +evaluate(group));
                 break;
             }
         }
     }
 
-    let left = arr.slice(0, opIndex);
-    let right = arr.slice(opIndex + 1);
-    let oper = arr[opIndex];
+    if (arr.length < 2) {
+        return arr[0];
+    }
 
-    return operators.operate(oper, +eval2(left), +eval2(right));
-}
+    let index = 1;
 
-const test2 = function (func) {
-    console.log(func);
+    if (arr.includes('add') || arr.includes('subtract')) {
+        for (let i = index; i < arr.length; i++) {
+            let item = arr[i];
+            if (item == 'add' || item == 'subtract') {
+                index = i;
+                break;
+            }
+        }
+    }
+
+    let left = arr.slice(0, index);
+    let right = arr.slice(index + 1);
+    let oper = arr[index];
+
+    return operators.operate(oper, +evaluate(left), +evaluate(right));
 }
